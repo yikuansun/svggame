b922a645acfa298889d118a92cccd82ccf174780
Yikuan Sun <yikuansun2015@gmail.com>
2020-12-09 13:33:39 -0500

    made a group for scrolling

--- code.js
+++ code.js
@@ -9,19 +9,22 @@ function buildPlatform(x, y, width, height) {
     platform.style.fill = "rgb(0, 120, 0)";
     platform.setAttribute("width", width); platform.setAttribute("height", height);
     platform.setAttribute("x", x); platform.setAttribute("y", y);
-    document.getElementById("gameframe").appendChild(platform);
+    scrollelems.appendChild(platform);
     platform.setAttribute("class", "platform");
 }
 
 function levelInit(playerX, playerY, platformsMap) {
     document.getElementById("gameframe").style.backgroundColor = "deepskyblue";
 
+    scrollelems = document.createElementNS(svgns, "g");
+    document.getElementById("gameframe").appendChild(scrollelems);
+
     playerRect = document.createElementNS(svgns, "rect");
     playerRect.style.fill = "rgb(0, 20, 100)";
     playerRect.setAttribute("width", 50); playerRect.setAttribute("height", 50);
     playerCoords = [playerX, playerY];
     playerRect.setAttribute("x", playerX); playerRect.setAttribute("y", playerY);
-    document.getElementById("gameframe").appendChild(playerRect);
+    scrollelems.appendChild(playerRect);
 
     for (platform of platformsMap) {
         buildPlatform(platform.x, platform.y, platform.width, platform.height)
@@ -47,10 +50,10 @@ function touching_direction(rect1, rect2) {
     l_collision = rect1_right - parseFloat(rect2.getAttribute("x"));
     r_collision = rect2_right - parseFloat(rect1.getAttribute("x"));
     if (t_collision < b_collision && t_collision < l_collision && t_collision < r_collision) {
-        return "top";
+        return "bottom";
     }
     if (b_collision < t_collision && b_collision < l_collision && b_collision < r_collision) {
-        return "bottom";
+        return "top";
     }
     if (l_collision < r_collision && l_collision < t_collision && l_collision < b_collision) {
         return "left";
@@ -81,12 +84,12 @@ function detect_platform_collisions() {
                 playerCoords[0] =  parseFloat(platform.getAttribute("x")) + parseFloat(platform.getAttribute("width"));
                 velocity_right = 0;
             }
-            else if (collision == "top") {
-                out.touching_top = true;
-                playerCoords[1] = parseFloat(platform.getAttribute("y")) + parseFloat(platform.getAttribute("height"));
-            }
             else if (collision == "bottom") {
                 out.touching_bottom = true;
+                playerCoords[1] = parseFloat(platform.getAttribute("y")) + parseFloat(platform.getAttribute("height"));
+            }
+            else if (collision == "top") {
+                out.touching_top = true;
                 playerCoords[1] = parseFloat(platform.getAttribute("y")) - parseFloat(playerRect.getAttribute("height"));
             }
         }
@@ -97,16 +100,10 @@ function detect_platform_collisions() {
 
 function setscrolling(playerxpos, levelwidth) {
     if ((-(playerxpos - 426)) < 0 && (-(playerxpos - 426)) > -levelwidth + 852) {
-        for (platform of document.getElementsByClassName("platform")) {
-            platform.style.transform = "translateX(" + (-(playerxpos - 426)).toString() + "px)";
-        }
-        playerRect.style.transform = "translateX(" + (-(playerxpos - 426)).toString() + "px)";
+        scrollelems.setAttribute("transform", "translate(" + (-(playerxpos - 426)).toString() + ", 0)");
     }
     else {
-        playerRect.style.transform = "translateX(" + (((-(playerxpos - 426)) > 0)?0:-levelwidth + 852).toString() + "px)";
-        for (platform of document.getElementsByClassName("platform")) {
-            platform.style.transform = "translateX(" + (((-(playerxpos - 426)) > 0)?0:-levelwidth + 852).toString() + "px)";
-        }
+        scrollelems.setAttribute("transform", "translate(" + (((-(playerxpos - 426)) > 0)?0:-levelwidth + 852).toString() + ", 0)");
     }
 }
 
@@ -121,15 +118,15 @@ function load() {
 
     collisions = detect_platform_collisions();
 
-    if (collisions.touching_top) {
+    if (collisions.touching_bottom) {
         velocity_up = -1;
     }
     
-    if (!(collisions.touching_bottom)) {
+    if (!(collisions.touching_top)) {
         velocity_up = velocity_up - 0.5;
     }
     else {
-        if (map[88] && !(collisions.touching_top)) {
+        if (map[88] && !(collisions.touching_bottom)) {
             velocity_up = 8;
         }
         else {
