26d4f3cecf9449177038fd6e7e583dbb317fbc60
Yikuan Sun <yikuansun2015@gmail.com>
2021-02-15 14:03:51 -0500

    Update code.js

--- code.js
+++ code.js
@@ -63,15 +63,24 @@ function load() {
     }
     
     velocity_up = velocity_up - 0.5;
-    if (map[88] && touching_rect_polygon(playerRect, groundPolygon)) {
-        velocity_up = 8;
-    }
-    
+    var slope = 0;
     while (touching_rect_polygon(playerRect, groundPolygon)) {
         velocity_up = 0;
         playerRect.setAttribute("y", parseFloat(playerRect.getAttribute("y")) - 0.5);
         playerCoords[1] -= 0.5;
+        slope += 0.5;
+        if (slope > 8) {
+            playerRect.setAttribute("y", parseFloat(playerRect.getAttribute("y")) + slope);
+            playerCoords[1] += slope;
+            velocity_right = 0;
+            break;
+        }
     }
+    if (map[88] && touching_rect_polygon(playerRect, groundPolygon)) {
+        velocity_up = 8;
+        console.log("hi")
+    }
+    console.log(map[88])
     playerCoords[1] -= velocity_up;
     
     if (map[39]) {
